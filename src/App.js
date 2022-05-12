fa8f9c0bb00851e52eaf42378e7e060191a4b8a3
Akarshgu <akarshgupta705415@gmail.com>
2022-05-12 07:08:27 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+// creating a function component
+const Navbar=(props)=>{
+  
+  return (<>
+  <h1>i am nav  {props.name}  </h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Consequatur earum quas modi explicabo voluptates quam temporibus, 
+    perferendis eum voluptas esse, minima impedit beatae.
+     Rem tenetur repellendus facere architecto eum molestias.</p>
+  </>)
 }
 
-export default App;
+ // time ko screen p laane ke liye 
+const Time=()=>{
+const time=new Date().toLocaleTimeString();
+return <h1>Time:{time} </h1>
+}
+
+ // render ko root me rakh denge hr second chlne k liye 
+setInterval(() => {
+
+root.render(
+  <>
+    <App />
+    <input type="text"/>
+    <Navbar name='dinesh'/>
+    <Time/>
+  </>
+)
+}, 1000);
